25660a834536420d1c84a58150274ba54a1c9cb7
Maithri <maithri.krishna94@gail.com>
2020-07-20 17:55:42 +0530

    add meta description to improve SEO

--- src/template.js
+++ src/template.js
@@ -3,6 +3,10 @@ export default function template(title, content = "", preloadedState) {
               <html lang="en">
               <head>
                 <meta charset="utf-8">
+                <meta name="description" content="Hacker News is a community started by Paul Graham for sharing Anything that good
+                hackers would find interesting. That includes more than hacking and startups. If you
+                had to reduce it to a sentence, the answer might be: anything that gratifies one's
+                intellectual curiosity.">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title> ${title} </title>
                 <link href="/hackerNews/web/index.css" rel="stylesheet">
